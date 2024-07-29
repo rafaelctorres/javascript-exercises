a811695cb21f1391c5655906141c8dd75ee52d9c
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-29 17:35:36 -0300

    Solve findOldest and getTitles exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,41 @@
-const findTheOldest = function() {
-
-};
-
-// Do not edit below this line
-module.exports = findTheOldest;
+const findTheOldest = function(people) {
+  
+    let oldest = people.reduce(function (sum,current){
+      let ageCurrent;
+      let ageOldest;
+      
+      ageCurrent = "yearOfDeath" in current ? current.yearOfDeath - current.yearOfBirth : 2024 - current.yearOfBirth;
+      ageOldest = "yearOfDeath" in sum ? sum.yearOfDeath - sum.yearOfBirth : 2024 - sum.yearOfBirth;
+      
+      if (ageCurrent>ageOldest){
+        return current;
+      } else { 
+        return sum;
+      }
+    }, {yearOfBirth: 0, yearOfDeath: 0})
+    return oldest;
+    };
+    
+    
+    const peopleTest = [
+          {
+            name: "Carly",
+            yearOfBirth: 1942,
+            yearOfDeath: 1970,
+          },
+          {
+            name: "Ray",
+            yearOfBirth: 1962,
+            yearOfDeath: 2011,
+          },
+          {
+            name: "Jane",
+            yearOfBirth: 1912,
+            yearOfDeath: 1941,
+          },
+        ]
+    
+    console.log(findTheOldest(peopleTest));
+    
+    // Do not edit below this line
+    module.exports = findTheOldest;
