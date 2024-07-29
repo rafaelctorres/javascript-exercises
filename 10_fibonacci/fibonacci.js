75a246a7b0bb8bc0d349d33c38e9a85e08323877
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-29 17:30:10 -0300

    Solve fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    if( n < 0 ) return "OOPS"
+    let initial = 1;
+    let next = 0;
+    let result = 0;
+    for (let i = 0; i < n; i++){
+      result = initial + next;
+      initial = next;
+      next = result;
+    }
+    return result;
 };
-
-// Do not edit below this line
+  
+  console.log(fibonacci(6));
+  
+  
+  
+  // Do not edit below this line
 module.exports = fibonacci;
