80dc5905129761a7d6b691bbad7f12a0e6f8451c
Rafael Torres <rafaeltorres257@gmail.com>
2024-07-29 16:51:55 -0300

    Solve calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+  if (a>=0 && b>=0){
+    return a+b; 
+  }
+  else {
+    return ("please use positive numbers");
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let reduced = arr.reduce(function(sum, current) {
+     return sum + current;
+    }, 0);
+  return reduced;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let reduced = arr.reduce((sum, current) => sum*current);
+  return reduced;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let result = a === 0 ? 1 : a;
+	for (let i = a-1; i>0; i--){
+    result = result*i;
+  }
+  return result
 };
 
 // Do not edit below this line
